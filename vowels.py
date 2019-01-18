0f2e822acee568093c0d4ec6972d5eae76d3890f
ordinater <46736333+ordinater@users.noreply.github.com>
2019-01-18 16:45:23 -0500

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -4,7 +4,8 @@ VOWELS = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 def vowel_count(word):
     counter = 0
     for char in word:
-        if char in VOWELS: counter +=1
+        if char in VOWELS: 
+            counter +=1
 
     return counter
 
